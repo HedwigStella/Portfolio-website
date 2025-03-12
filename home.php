4ed650573971582e2ee54d925e285b47bcdfdde6
HedwigStella <hedwiglejeune@gmail.com>
2025-03-12 13:03:37 +0100

    Create home.php
    eerste commit, kale website zonder invulling of aankleding

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+// index.php
+
+// Basis HTML-structuur
+?><!DOCTYPE html>
+<html lang="Portfolio Hedwig Lejeune">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Hedwig Lejeune</title>
+</head>
+<body>
+    <h1>Hier komt het portfolio</h1>
+    <p>onderverdeling bv mijn python opdrachten</p>
+</body>
+</html>
